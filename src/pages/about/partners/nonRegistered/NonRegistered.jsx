1520711ce90679210682bc7152b6dcbb83d05e1d
jeliaaa <julia.aleqsandre@gmail.com>
2023-08-24 18:25:17 +0400

    partners

--- src/pages/about/partners/nonRegistered/NonRegistered.jsx
+++ src/pages/about/partners/nonRegistered/NonRegistered.jsx
@@ -8,7 +8,7 @@ const NonRegistered = () => {
     <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
       <Breadcrumbs></Breadcrumbs>
       <div>
-        <PartnersCard name={'Spar'} thumb={'https://picsum.photos/200/100'} img={'https://www.nbi.org.za/wp-content/uploads/2020/07/SPAR-logo.jpg'} />
+        <PartnersCard name={'Spar'} thumb={'https://picsum.photos/200/100'} img={'https://www.nbi.org.za/wp-content/uploads/2020/07/SPAR-logo.jpg'} link={'https://spargeorgia.com/'} />
       </div>
     </Container>
 
