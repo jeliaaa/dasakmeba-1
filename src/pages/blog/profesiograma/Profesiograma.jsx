a07d53b66457d0b31b2cee464acc4ffdd7e4ec71
Mishka Tenieshvili <59793581+Mishkaten@users.noreply.github.com>
2023-08-24 18:23:42 +0400

    some more

--- src/pages/blog/profesiograma/Profesiograma.jsx
+++ src/pages/blog/profesiograma/Profesiograma.jsx
@@ -3,6 +3,12 @@ import { useTranslation } from 'react-i18next'
 import { Container } from 'react-bootstrap';
 import Breadcrumbs from '../../../ReusableComponents/breadcrumbs/Breadcrumbs';
 import ProfesiogramaCard from '../../../ReusableComponents/profesiograma/ProfesiogramaCard';
+import itPhoto from "../../../assets/ProfesiogramAssets/IT.jpg"
+import construction from "../../../assets/ProfesiogramAssets/Construct.jpg"
+import servicing from "../../../assets/ProfesiogramAssets/Servicing.jpg"
+
+
+
 
 const Profesiograma = () => {
   const { t } = useTranslation();
@@ -10,9 +16,9 @@ const Profesiograma = () => {
     <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
       <Breadcrumbs></Breadcrumbs>
       <div className='profesiograma_wrapper' style={{ display: 'flex', justifyContent: 'flex-start', gap: '20px', flexWrap: 'wrap' }}>
-        <ProfesiogramaCard img={'https://picsum.photos/300/200'} name={t('it')} id={1} />
-        <ProfesiogramaCard img={'https://picsum.photos/300/200'} name={t('physical')} id={2} />
-        <ProfesiogramaCard img={'https://picsum.photos/300/200'} name={t('servicing')} id={3} />
+        <ProfesiogramaCard img={itPhoto} name={t('it')} id={1} />
+        <ProfesiogramaCard img={construction} name={t('physical')} id={2} />
+        <ProfesiogramaCard img={servicing} name={t('servicing')} id={3} />
         <ProfesiogramaCard img={'https://picsum.photos/300/200'} name={t('lorem')} id={4} />
         <ProfesiogramaCard img={'https://picsum.photos/300/200'} name={t('ipsum')} id={5} />
       </div>
