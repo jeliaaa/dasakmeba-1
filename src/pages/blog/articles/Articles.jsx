47138c3510848e10083ebea26263152a9c848015
Mishka Tenieshvili <59793581+Mishkaten@users.noreply.github.com>
2023-08-25 20:14:07 +0400

    shevavse kvela page romlis design gaketebuli iko

--- src/pages/blog/articles/Articles.jsx
+++ src/pages/blog/articles/Articles.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import Breadcrumbs from '../../../ReusableComponents/breadcrumbs/Breadcrumbs';
+import InfoCard from '../../../ReusableComponents/infocard/InfoCard';
+import "../../media/news/News.scss"
 
 const Articles = () => {
   return (
-    <Container style={{backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px'}}>
-    <Breadcrumbs></Breadcrumbs>
-    <div>Articles</div>
+    <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
+      <Breadcrumbs></Breadcrumbs>
+      <div className='news_wrapper'>
+        <InfoCard date={'აგვ. 23, 2023'} heading={'სტატია 1'} thumbText={'დაინტერესების შემთხვევაში, გთხოვთ, ელექტრონული ფოსტის მისამართზე'} />
+        <InfoCard date={'ივლ. 12, 2023'} heading={'სტატია 2'} thumbText={'გამოაგზავნოთ დოკუმენტაცია მოთხოვნილი ფორმით და ელექტრონული ფოსტის სათაურის ველში მიუთითოთ'} />
+        <InfoCard date={'მაი. 05, 2023'} heading={'სტატია 3'} thumbText={'პროფესიული განათლებისთვის“ ფარგლებში და მიზნად ისახავს კიბერუსაფრთხოების დარგში კვალიფიციური'} />
+      </div>
     </Container>
-
   )
 }
 
