c6f03230c157d0439760fbbf36463287ca9cbd94
Mishka Tenieshvili <59793581+Mishkaten@users.noreply.github.com>
2023-08-25 18:53:25 +0400

    minor updates

--- src/pages/blog/videoLessons/VideoLessons.jsx
+++ src/pages/blog/videoLessons/VideoLessons.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Container } from 'react-bootstrap';
 import Breadcrumbs from '../../../ReusableComponents/breadcrumbs/Breadcrumbs';
+import ProfesiogramaCard from '../../../ReusableComponents/profesiograma/ProfesiogramaCard';
 
 const VideoLessons = () => {
+  const { t } = useTranslation();
   return (
-    <Container style={{backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px'}}>
-    <Breadcrumbs></Breadcrumbs>
-    <div>VideoLessons</div>
+    <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
+      <Breadcrumbs></Breadcrumbs>
+      <div className='profesiograma_wrapper' style={{ display: 'flex', justifyContent: 'flex-start', gap: '20px', flexWrap: 'wrap' }}>
+        <ProfesiogramaCard img={"https://picsum.photos/300/200"} name={t('მიმართულება 1')} id={1} />
+        <ProfesiogramaCard img={"https://picsum.photos/300/200"} name={t('მიმართულება 2')} id={2} />
+        <ProfesiogramaCard img={"https://picsum.photos/300/200"} name={t('მიმართულება 3')} id={3} />
+        <ProfesiogramaCard img={"https://picsum.photos/300/200"} name={t('მიმართულება 4')} id={4} />
+        <ProfesiogramaCard img={"https://picsum.photos/300/200"} name={t('მიმართულება 5')} id={5} />
+      </div>
     </Container>
-
   )
 }
 
