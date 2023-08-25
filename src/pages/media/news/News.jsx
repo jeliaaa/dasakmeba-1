f3c2f1c732ed786a81361d2b6577881298bf7d7b
jeliaaa <julia.aleqsandre@gmail.com>
2023-08-25 18:52:37 +0400

    infocard

--- src/pages/media/news/News.jsx
+++ src/pages/media/news/News.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import Breadcrumbs from '../../../ReusableComponents/breadcrumbs/Breadcrumbs';
+import InfoCard from '../../../ReusableComponents/infocard/InfoCard';
 
 const News = () => {
   return (
     <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
       <Breadcrumbs></Breadcrumbs>
-      <div>News</div>
+      <div>
+        <InfoCard date={'აგვ. 23, 2023'} heading={'News 1'} thumbText={'NEWS lorem lorem lorem lorem lorem lorem ,orem NEws NWew'} />
+      </div>
     </Container>
 
   )
