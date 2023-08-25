ff9c9eb84568da5e2c9ecdd88766d2f5bad39bb1
jeliaaa <julia.aleqsandre@gmail.com>
2023-08-25 23:39:31 +0400

    calendar

--- src/pages/blog/calendar/months/Jan.jsx
+++ src/pages/blog/calendar/months/Jan.jsx
@@ -6,13 +6,13 @@ import Breadcrumbs from '../../../../ReusableComponents/breadcrumbs/Breadcrumbs'
 const Jan = () => {
     const { t } = useTranslation();
     return (
-        <Container style={{ backgroundColor: '#fff', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 80 }}>
+        <Container style={{ backgroundColor: '#fff', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 80, borderRadius: '8px' }}>
             <Breadcrumbs />
-            <Accordion defaultActiveKey="0" flush style={{ width: '80%', border:'1px solid black' }}>
+            <Accordion defaultActiveKey="0" flush style={{ width: '80%', border: '1px solid black' }}>
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>14 {t('jan')}</Accordion.Header>
                     <Accordion.Body>
-                        ქართული თეატრის დღეს საფუძველი ჩაეყარა 1850 წლის 14 იანვარს, როდესაც თბილისში პირველი სპექტაკლი გიორგი ერისთავის „გაყრა“ დაიდგა. 
+                        ქართული თეატრის დღეს საფუძველი ჩაეყარა 1850 წლის 14 იანვარს, როდესაც თბილისში პირველი სპექტაკლი გიორგი ერისთავის „გაყრა“ დაიდგა.
                         ეს იყო პირველი და იმ დროისთვის ერთადერათი პროფესიული თეატრი საქართველოში.
                     </Accordion.Body>
                 </Accordion.Item>
