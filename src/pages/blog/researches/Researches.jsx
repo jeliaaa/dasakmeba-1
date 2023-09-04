d345dd4e61f409173cae28a1cdc4d93364040ba6
jeliaaa <julia.aleqsandre@gmail.com>
2023-09-05 01:58:32 +0400

    researches updated

--- src/pages/blog/researches/Researches.jsx
+++ src/pages/blog/researches/Researches.jsx
@@ -1,19 +1,50 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 import Breadcrumbs from '../../../ReusableComponents/breadcrumbs/Breadcrumbs';
 import InfoCard from '../../../ReusableComponents/infocard/InfoCard';
 import "../../media/news/News.scss"
-
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+const statsData = [
+  { id: 1, name: 'კვლევა N1', to: '/dummy.pdf', date: '12.01.2021' },
+  { id: 2, name: 'კვლევა N2', to: '/dummy.pdf', date: '21.02.2021' },
+  { id: 3, name: 'კვლევა N3', to: '/dummy.pdf', date: '21.03.2021' },
+  { id: 4, name: 'კვლევა N4', to: '/dummy.pdf', date: '21.04.2021' },
+  { id: 5, name: 'კვლევა N5', to: '/dummy.pdf', date: '21.05.2021' },
+  { id: 6, name: 'კვლევა N6', to: '/dummy.pdf', date: '21.06.2021' },
+  { id: 7, name: 'კვლევა N7', to: '/dummy.pdf', date: '21.07.2021' },
+  { id: 8, name: 'კვლევა N8', to: '/dummy.pdf', date: '21.08.2021' },
+  { id: 9, name: 'კვლევა N9', to: '/dummy.pdf', date: '21.09.2021' },
+  { id: 10, name: 'კვლევა N10', to: '/dummy.pdf', date: '21.10.2021' }
+]
 const Researches = () => {
+  const { t } = useTranslation();
   return (
-    <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
-      <Breadcrumbs></Breadcrumbs>
-      <div className='news_wrapper'>
-        <InfoCard id={1} date={'აგვ. 23, 2023'} heading={'კვლევა 1'} thumbText={'დაინტერესების შემთხვევაში, გთხოვთ, ელექტრონული ფოსტის მისამართზე'} />
-        <InfoCard id={2} date={'ივლ. 12, 2023'} heading={'კვლევა 2'} thumbText={'გამოაგზავნოთ დოკუმენტაცია მოთხოვნილი ფორმით და ელექტრონული ფოსტის სათაურის ველში მიუთითოთ'} />
-        <InfoCard id={3} date={'მაი. 05, 2023'} heading={'კვლევა 3'} thumbText={'პროფესიული განათლებისთვის“ ფარგლებში და მიზნად ისახავს კიბერუსაფრთხოების დარგში კვალიფიციური'} />
-      </div>
-    </Container>
+    <div className='researches_wrapper' style={{backgroundColor:'#fff', padding:'40px'}}>
+      <Breadcrumbs />
+      <Container style={{ backgroundColor: '#fff' }}>
+        <Table responsive>
+          <thead>
+            <tr>
+              <th></th>
+              <th>{t('name')}</th>
+              <th>{t('date')}</th>
+              <th>{t('view')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {statsData.map((file) => (
+              <tr key={file.id}>
+                <td>{file.id}</td>
+                <td>{file.name}</td>
+                <td>{file.date}</td>
+                <td>{<Link to={file.to} target="_blank" download>{file.name}</Link>}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
   )
 }
 
