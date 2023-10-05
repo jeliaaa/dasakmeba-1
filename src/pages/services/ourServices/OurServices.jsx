3f8e48b03008f291a61879c9956d11db22cfc022
Mishka Tenieshvili <59793581+Mishkaten@users.noreply.github.com>
2023-10-05 10:28:26 +0400

    Banners Vol2

--- src/pages/services/ourServices/OurServices.jsx
+++ src/pages/services/ourServices/OurServices.jsx
@@ -4,14 +4,18 @@ import { useTranslation } from 'react-i18next'
 import { Container } from 'react-bootstrap';
 import Breadcrumbs from '../../../ReusableComponents/breadcrumbs/Breadcrumbs';
 
+import ForOrgBanner from '../../../assets/NavBoxBanners/Services/Ourservices/Group 29.png';
+import ForUserBanner from '../../../assets/NavBoxBanners/Services/Ourservices/Group 28.png';
+
+
 const OurServices = () => {
   const { t } = useTranslation();
   return (
     <Container style={{ backgroundColor: '#fff', padding: '20px 20px', borderRadius: '8px' }}>
       <Breadcrumbs></Breadcrumbs>
       <div className='services_wrapper' style={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
-        <NavBox name={t('forOrg')} to={'/services/ourServices/forOrgs'} />
-        <NavBox name={t('forUsers')} to={'/services/ourServices/forUsers'} />
+        <NavBox img={ForOrgBanner} name={t('forOrg')} to={'/services/ourServices/forOrgs'} />
+        <NavBox img={ForUserBanner} name={t('forUsers')} to={'/services/ourServices/forUsers'} />
 
       </div>
     </Container>
